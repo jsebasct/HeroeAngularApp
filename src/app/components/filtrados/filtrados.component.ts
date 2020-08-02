1d677e2617ed1c4c85b9c5b2c5fdb6554819770d
jsebasct <jsebasct@gmail.com>
2020-08-02 16:15:01 -0300

    feat: no hay resultados

--- src/app/components/filtrados/filtrados.component.ts
+++ src/app/components/filtrados/filtrados.component.ts
@@ -4,21 +4,24 @@ import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-filtrados',
-  //templateUrl: './filtrados.component.html',
-  templateUrl: '../heroes/heroes.component.html',
-  styleUrls: ['./filtrados.component.css']
+  templateUrl: './filtrados.component.html',
+  // templateUrl: '../heroes/heroes.component.html',
 })
 export class FiltradosComponent implements OnInit {
 
   heroes: Heroe[] = []
+  termino: string
 
   constructor(private activatedRoute: ActivatedRoute
               , private heroesService: HeroesService) {   
 
     this.activatedRoute.params.subscribe( params => {
+
       console.log('nombre en filtrados: ', params['name'])
+      this.termino = params['name']
       this.heroes = heroesService.buscarHeroes(params['name'])
-      // console.log('heroe', this.heroe)
+
+      console.log('encontrados: ', this.heroes)
     })
   }
 
